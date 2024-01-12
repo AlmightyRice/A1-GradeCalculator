d5a85fc083fc2faaddb590afb209ded6a89b129a
AlmightyRice <156128490+AlmightyRice@users.noreply.github.com>
2024-01-11 22:45:17 -0700

    Add files via upload

---
+++ A1-GradeCalculator.py
@@ -0,0 +1,9 @@
+# User input
+name = input("Please enter your name")
+Assignments = float(input( "Enter your Programming Assignments: "))
+midterm_1 = float(input("Enter Midterm 1 Percentage: "))
+midterm_2 = float(input("Enter Midterm 1 Percentage: "))
+quizzes = float(input("Enter Quizz Percentage: "))
+Ebook = float(input("Enter Ebook Percentage: "))
+Participation = float(input(" Enter Participation Percentage: "))
+
