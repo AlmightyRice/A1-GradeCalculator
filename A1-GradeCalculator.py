d59b9569904df4eef7ca8c0b724061c729619f5a
AlmightyRice <destrojhn@gmail.com>
2024-01-12 13:46:17 -0700

    New prints

--- A1-GradeCalculator.py
+++ A1-GradeCalculator.py
@@ -1,9 +1,16 @@
 # User input
-name = input("Please enter your name")
+name = input("Please enter your name: ")
 Assignments = float(input( "Enter your Programming Assignments: "))
-midterm_1 = float(input("Enter Midterm 1 Percentage: "))
-midterm_2 = float(input("Enter Midterm 1 Percentage: "))
-quizzes = float(input("Enter Quizz Percentage: "))
+Midterm_1 = float(input("Enter Midterm 1 Percentage: "))
+Midterm_2 = float(input("Enter Midterm 1 Percentage: "))
+Quizzes = float(input("Enter Quizz Percentage: "))
 Ebook = float(input("Enter Ebook Percentage: "))
-Participation = float(input(" Enter Participation Percentage: "))
-#New function
+Participation = float(input("Enter Participation Percentage: "))
+# Add score
+add_two = Assignments + Midterm_1
+print(add_two)
+print('Rae this is your score ' + str(add_two))
+add_up = Assignments +Midterm_1 + Midterm_2 + Quizzes +Ebook +Participation
+final_score = add_up/100
+print(final_score)
+print( name + " This is your final score " + str(final_score))
