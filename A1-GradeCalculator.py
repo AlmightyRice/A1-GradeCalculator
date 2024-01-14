909089f00e5cdf103a3925e716e73ada443f91c5
AlmightyRice <destrojhn@gmail.com>
2024-01-13 22:35:46 -0700

    new

--- A1-GradeCalculator.py
+++ A1-GradeCalculator.py
@@ -8,16 +8,18 @@ Ebook = float(input("Enter Ebook Percentage: "))
 Participation = float(input("Enter Participation Percentage: "))
 
 # course percentage
-assignment_weight = 0.30
-midterm_weight = .20
-final_weight = .15
-class_quizz_weight = .15
-lab_weight = .15
-Ebook_weight = .10
-final_percentage = (Programming_Assignments * assignment_weight + Midterm * midterm_weight + In_class_quizzes * class_quizz_weight + Lab_quizzes * lab_weight + Ebook
-* Ebook_weight)
-
-# Compute
 for final_exam_score in range(0, 101, 20):
-    score = final_exam_score * .15
-    print( Name + ", If your final exam score is " + str(final_exam_score) + " then your course percentage for Cs 1400 will be " + str(final_percentage))
+    assignment_weight = 0.30
+    midterm_weight = 0.20
+    final_weight = 0.15
+    class_quizz_weight = 0.15
+    lab_weight = 0.15
+    Ebook_weight = 0.10
+
+    # Compute
+    final_percentage = (Programming_Assignments * assignment_weight + Midterm * midterm_weight + In_class_quizzes * class_quizz_weight + Lab_quizzes * lab_weight + Ebook
+                * Ebook_weight + final_exam_score * final_weight)
+
+
+
+    print( Name + ', If your final exam score is ' + str(final_exam_score) + " then your course percentage for Cs 1400 will be " + str(final_percentage))
