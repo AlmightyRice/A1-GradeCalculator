b0f501b1efafbee0edc7eb2fa015f676e7260de6
AlmightyRice <destrojhn@gmail.com>
2024-01-13 21:32:36 -0700

    Final

--- A1-GradeCalculator.py
+++ A1-GradeCalculator.py
@@ -1,17 +1,23 @@
 # User input
-name = input("Please enter your name: ")
-Assignments = float(input( "Enter your Programming Assignments: "))
-Midterm_1 = float(input("Enter Midterm 1 Percentage: "))
-Midterm_2 = float(input("Enter Midterm 1 Percentage: "))
-Quizzes = float(input("Enter Quizz Percentage: "))
+Name = input("Please enter your name: ")
+Programming_Assignments = float(input("Enter your Programming Assignments: "))
+Midterm = float(input("Enter Midterm Percentage: "))
+In_class_quizzes = float(input("Enter Quizz Percentage: "))
+Lab_quizzes = float(input("Enter Lab quiz Percentage: "))
 Ebook = float(input("Enter Ebook Percentage: "))
 Participation = float(input("Enter Participation Percentage: "))
-# Add score
-add_two = Assignments + Midterm_1
-print(add_two)
-print('Rae this is your score ' + str(add_two))
-add_up = Assignments +Midterm_1 + Midterm_2 + Quizzes +Ebook +Participation
-final_score = add_up/100
-print(final_score)
-print( name + " This is your final score " + str(final_score))
-print(90)
+
+# course percentage
+assignment_weight = 0.30
+midterm_weight = .20
+final_weight = .15
+class_quizz_weight = .15
+lab_weight = .15
+Ebook_weight = .10
+final_percentage = (Programming_Assignments * assignment_weight + Midterm * midterm_weight + In_class_quizzes * class_quizz_weight + Lab_quizzes * lab_weight + Ebook
+* Ebook_weight)
+
+# Compute
+for final_exam_score in range(0, 101, 20):
+    score = final_exam_score * .15
+    print( Name + ", If your final exam score is " + str(final_exam_score) + " then your course percentage for Cs 1400 will be " + str(final_percentage))
